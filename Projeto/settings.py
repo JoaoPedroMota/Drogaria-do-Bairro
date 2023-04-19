f49c1e701e08d07432b49315789403c0d0046a60
migueldeoleiros <migueldeoleiros@gmail.com>
2023-04-19 11:03:22 +0100

    Add connection to cloud SQL
    Do not merger this into developing branches, the database won't work on
    local machines

--- Projeto/settings.py
+++ Projeto/settings.py
@@ -92,8 +92,12 @@ WSGI_APPLICATION = 'Projeto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db-drogaria',
+        'USER': 'user1',
+        'PASSWORD': '12345',
+        'HOST': '34.175.141.4',
+        'PORT': '5432',
     }
 }
 
