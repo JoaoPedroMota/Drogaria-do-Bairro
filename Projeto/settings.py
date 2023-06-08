80c0e062ec0fd978f39ca4dbbc539f383b24431e
João Pedro Mota <fc54940@alunos.fc.ul.pt>
2023-06-08 17:25:02 +0100

    alterar callback view, para return redirect('loja-home')

--- Projeto/settings.py
+++ Projeto/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost", "34.0.207.66", "drogariadobairro.pt"]
 
-CSRF_TRUSTED_ORIGINS = ['https://drogariadobairro.pt', "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ['https://drogariadobairro.pt', "http://127.0.0.1:8000"]
 
 # Application definition
 
