e21bde4bdf3f216dd89d92f0fc649c6b1dab84ab
migueldeoleiros <migueldeoleiros@gmail.com>
2023-06-22 08:52:34 +0100

    Use local db if no ip provided

--- Projeto/settings.py
+++ Projeto/settings.py
@@ -118,23 +118,27 @@ WSGI_APPLICATION = 'Projeto.wsgi.application'
 
 #Database
 #https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': DB_IP,
-        'PORT': '5432',
-        'NAME': 'drogaria_db',
-        'USER': DB_USER,
-        'PASSWORD': DB_PASSWORD,
-        'OPTIONS': {
-            "options": "-c search_path=public"
+if DB_IP:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'HOST': DB_IP,
+            'PORT': '5432',
+            'NAME': 'drogaria_db',
+            'USER': DB_USER,
+            'PASSWORD': DB_PASSWORD,
+            'OPTIONS': {
+                "options": "-c search_path=public"
+            }
         }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
