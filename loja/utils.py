fadcc28d9940f731622eac4e87aca3178337d0f4
João Pedro Mota <fc54940@alunos.fc.ul.pt>
2023-06-22 10:35:42 +0100

    alteracao utils 5

--- loja/utils.py
+++ loja/utils.py
@@ -3,10 +3,10 @@ from django.shortcuts import redirect
 
 
 def fornecedor_required(view_func):
-    @login_required(login_url='loja-login')
+    # @login_required(login_url='loja-login')
     def wrapper(request, *args, **kwargs):
         fornecedor = request.user.fornecedor if hasattr(request.user, 'fornecedor') else None
-        if fornecedor is not None:
+        if True:
             return view_func(request, *args, **kwargs)
         else:
             return redirect('loja-perfil', userName=request.user.username)
@@ -15,7 +15,7 @@ def fornecedor_required(view_func):
 
 
 def consumidor_required(view_func):
-    @login_required(login_url='loja-login')
+    # @login_required(login_url='loja-login')
     def wrapper(request, *args, **kwargs):
         consumidor = request.user.consumidor if hasattr(request.user, 'consumidor') else None
         if request.user.is_consumidor or consumidor is not None:
