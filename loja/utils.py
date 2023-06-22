1f44a1d2f7fed72654babe8d87f453345703283c
João Pedro Mota <fc54940@alunos.fc.ul.pt>
2023-06-22 10:08:15 +0100

    alteração no utils 2

--- loja/utils.py
+++ loja/utils.py
@@ -18,7 +18,8 @@ def consumidor_required(view_func):
     @login_required(login_url='loja-login')
     def wrapper(request, *args, **kwargs):
         if request.user.is_consumidor:
-            return view_func(request, *args, **kwargs)
+            #return view_func(request, *args, **kwargs)
+            return True
         else:
             return redirect('loja-perfil', userName=request.user.username)
     return wrapper
